5e91005e126588c1f8b25949e2440c4fa0629ce0
Fred Lackey <fred.lackey@gmail.com>
2020-03-16 16:04:09 -0400

    initial create

---
+++ src/os/create_directories.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+create_directories() {
+
+    declare -a DIRECTORIES=(
+        "$HOME/Documents/Screenshots"
+        "$HOME/Source/Bitbucket/FredLackey"
+        "$HOME/Source/GitHub/FredLackey"
+    )
+
+    for i in "${DIRECTORIES[@]}"; do
+        mkd "$i"
+    done
+
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+    print_in_purple "\n • Create directories\n\n"
+    create_directories
+}
+
+main
