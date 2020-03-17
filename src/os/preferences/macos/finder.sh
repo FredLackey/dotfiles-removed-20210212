8e0ea9cbb8791ecf968f295cdef55d0cf2fa2a41
Fred Lackey <fred.lackey@gmail.com>
2020-03-17 07:59:32 -0400

    add macos apps

--- src/os/preferences/macos/finder.sh
+++ src/os/preferences/macos/finder.sh
@@ -8,6 +8,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 print_in_purple "\n   Finder\n\n"
 
 execute "defaults write com.apple.menuextra.clock DateFormat 'EEE MMM d  HHmm'" \
+
 # execute "defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true && \
 #          defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true && \
 #          defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true" \
@@ -44,8 +45,8 @@ execute "defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool t
 execute "defaults write com.apple.finder ShowRecentTags -bool false" \
     "Do not show recent tags"
 
-execute "defaults write -g AppleShowAllExtensions -bool true" \
-    "Show all filename extensions"
+# execute "defaults write -g AppleShowAllExtensions -bool true" \
+#     "Show all filename extensions"
 
 # execute "/usr/libexec/PlistBuddy -c 'Set :DesktopViewSettings:IconViewSettings:iconSize 72' ~/Library/Preferences/com.apple.finder.plist && \
 #          /usr/libexec/PlistBuddy -c 'Set :StandardViewSettings:IconViewSettings:iconSize 72' ~/Library/Preferences/com.apple.finder.plist" \
