63a04e6e8b6805cefdebce2c1998837ad784735f
Fred Lackey <fred.lackey@gmail.com>
2020-04-09 16:55:22 -0400

    create ubuntu server

--- src/os/install/ubuntu-18-svr/development.sh
+++ src/os/install/ubuntu-18-svr/development.sh
@@ -13,12 +13,24 @@ execute \
   "Docker (remove older)"
 
 execute \
-  "sudo apt install apt-transport-https ca-certificates curl software-properties-common gnupg \
-    && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - \
-    && sudo apt-key fingerprint 0EBFCD88 \
-    && sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" \
-    && sudo apt update" \
-  "Docker (add dependencies)"
+  "sudo apt install apt-transport-https ca-certificates curl software-properties-common gnupg" \
+  "Docker (add certificates)"
+
+execute \
+  "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -" \
+  "Docker (add keys)"
+
+execute \
+  "sudo apt-key fingerprint 0EBFCD88" \
+  "Docker (add fingerprint)"
+
+execute \
+  "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\"" \
+  "Docker (add repository)"
+
+execute \
+  "sudo apt update" \
+  "Docker (update)"
 
 install_package "Docker CE" "docker-ce"
 
