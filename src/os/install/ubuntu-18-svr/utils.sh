7dcb4050888d4f5295fec5e9fe07bff69385ac36
Fred Lackey <fred.lackey@gmail.com>
2020-04-09 16:19:11 -0400

    create ubuntu server

--- src/os/install/ubuntu-18-svr/utils.sh
+++ src/os/install/ubuntu-18-svr/utils.sh
@@ -66,9 +66,12 @@ upgrade() {
 
     # Install the newest versions of all packages installed.
 
+    # execute \
+    #     "export DEBIAN_FRONTEND=\"noninteractive\" \
+    #         && sudo apt-get -o Dpkg::Options::=\"--force-confnew\" upgrade -qqy" \
+    #     "APT (upgrade)"
     execute \
-        "export DEBIAN_FRONTEND=\"noninteractive\" \
-            && sudo apt-get -o Dpkg::Options::=\"--force-confnew\" upgrade -qqy" \
+        "sudo apt-get upgrade -qqy" \
         "APT (upgrade)"
 
 }
